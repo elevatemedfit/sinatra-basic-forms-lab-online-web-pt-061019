1a919d68e66f5528a91b9f1160bb9ae3030dccb6
Chris Junker <elevatemedfit@gmail.com>
2019-09-18 16:57:34 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -2,12 +2,11 @@
 
 class Puppy
 
-  attr_accessor  :name, :breed, :age, :months_old
+  attr_accessor  :name, :breed, :months_old
 
-  def initialize(name, breed, age, months_old)
+  def initialize(name, breed, months_old)
     @name = name
     @breed = breed
-    @age = age
     @months_old = months_old
   end
 
